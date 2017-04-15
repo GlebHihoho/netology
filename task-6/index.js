a8e4f3a80fc3ee04939fb0fac5e403783a46cdf8
Gleb Motuz <hihoho@mail.ru>
2017-04-15 12:22:06 +0200

    task-6.1
    первые шаги в експресс

--- task-6/index.js
+++ task-6/index.js
@@ -2,19 +2,47 @@
 
 const EXPRESS     = require('express');
 const BODY_PARSER = require('body-parser');
+const FS          = require('fs');
 const PORT        = 3000;
 
 const SERVER = EXPRESS();
 
-SERVER.post('/users/:parammm', (request, response) => {
-  let param = request.params.parammm;
-  response.json(param);
+const CONF  = { encoding: 'utf8' };
+const USERS = {
+  "name"      : "Gleb",
+  "last-name" : "Motuz"
+}
+
+
+SERVER.use(BODY_PARSER.json());
+SERVER.use(BODY_PARSER.urlencoded({"extended": true}));
+
+SERVER.post('/users/*', (request, response) => {
+  response.send(
+    FS.writeFile('users.json', '', CONF, () => {
+      console.log('Файл создан');
+    })
+  )
 });
 
-SERVER.get('/users/', (request, response) => {
+SERVER.put('/users/:name/:score', (request, response) => {
   let name = request.params.name;
   let score = request.params.score;
-  response.send(`Hello, You name ${name} and ${score}`);
+
+  response.send(
+    FS.appendFile('users.json', `{"name" : "${name}", "score" : "${score}"}`, CONF, () => {
+      console.log(`Свойство name и score добавлено`)
+    })
+  )
+});
+
+SERVER.get('/users/*', (request, response) => {
+  response.send(
+    FS.readFile('users.json', CONF, (err, data) => {
+      if (err) throw err;
+      console.log(`Файл содержит ${data}`);
+    })
+  )
 });
 
 SERVER.all('*', (request, response) => {
